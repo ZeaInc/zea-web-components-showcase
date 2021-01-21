1e511e913591182703381b819a383f554490d205
mauroporras <mauroporrasc@gmail.com>
2021-01-21 15:20:26 -0500

    fix: story for TreeView

--- stories/TreeView/TreeView.js
+++ stories/TreeView/TreeView.js
@@ -4,20 +4,47 @@ const { SelectionManager } = globalThis.zeaUx
 /**
  * Primary UI component for user interaction
  */
-export const TreeView = ({ highlightColor, enableHighlights, setSelected, nDeep }) => {
-  const colorData = highlightColor.replace('rgba(', '').replace(')', '').split(',').map(val => parseFloat(val))
+export const TreeView = ({
+  highlightColor,
+  enableHighlights,
+  setSelected,
+  nDeep,
+}) => {
+  const colorData = highlightColor
+    .replace('rgba(', '')
+    .replace(')', '')
+    .split(',')
+    .map((val) => parseFloat(val))
   const treeViewElement = document.createElement('zea-tree-view')
 
   const rootTreeItem = new TreeItem('MainItem')
   const selectionManager = new SelectionManager({}, {})
-  const color = new Color(colorData[0] / 255, colorData[1] / 255, colorData[2] / 255)
+
+  // Note: the alpha value determines  the fill of the highlight.
+  const selectionColor = new Color('#CEFF00')
+  selectionColor.a = 0.1
+  const subtreeColor = selectionColor.lerp(new Color(1, 1, 1, 0), 0.5)
+  selectionManager.selectionGroup
+    .getParameter('HighlightColor')
+    .setValue(selectionColor)
+  selectionManager.selectionGroup
+    .getParameter('SubtreeHighlightColor')
+    .setValue(subtreeColor)
+
+  const color = new Color(
+    colorData[0] / 255,
+    colorData[1] / 255,
+    colorData[2] / 255
+  )
   color.a = colorData[3]
 
   const addHighlights = (treeItem, index) => {
     let highlighted = false
 
     setInterval(() => {
-      !highlighted ? treeItem.addHighlight('test', color, true) : treeItem.removeHighlight('test', true)
+      !highlighted
+        ? treeItem.addHighlight('test', color, true)
+        : treeItem.removeHighlight('test', true)
       highlighted = !highlighted
     }, index * 500 + 500)
   }
@@ -39,10 +66,9 @@ export const TreeView = ({ highlightColor, enableHighlights, setSelected, nDeep 
     rootTreeItem.addChild(childI)
   }
 
-  if (setSelected) selectionManager.setSelection(new Set([ rootTreeItem ]))
+  if (setSelected) selectionManager.setSelection(new Set([rootTreeItem]))
   treeViewElement.items = [rootTreeItem, new TreeItem('Second')]
   treeViewElement.selectionManager = selectionManager
-  
 
   // setTimeout(() => {
   //   rootTreeItem.setName('NameChangeTest')
@@ -60,4 +86,4 @@ export const TreeView = ({ highlightColor, enableHighlights, setSelected, nDeep 
   // }, 5000)
 
   return treeViewElement
-};
+}
